f77ac2c11acf9bc35f452ba6ee63df196f3deb7f
gaowhen <gaowhen.com@gmail.com>
2016-12-12 15:21:46 +0800

    feat: update webpack; build path config

--- bin/webpack.dev.js
+++ bin/webpack.dev.js
@@ -3,6 +3,10 @@ const webpack = require('webpack')
 const StringReplacePlugin = require('string-replace-webpack-plugin')
 
 const config = require('./webpack.common')
+const gonConfig = require('./config')
+
+const domain = gonConfig.dev.domain
+const buildPath = `${gonConfig.asset.origin}${gonConfig.asset.build.origin}`
 
 const PATH = config.PATH
 const CWD = process.cwd()
@@ -27,8 +31,10 @@ module.exports = {
     // or webpack cannot resolve these modules
     // These aliasing is used when trying to resolve a module
     // alias: {
-    //   'webpack-hot-middleware/client': path.resolve(__dirname, '../node_modules/webpack-hot-middleware/client.js'),
-    //   'react-hot-loader/patch': path.resolve(__dirname, '../node_modules/react-hot-loader/patch.js'),
+    //   'webpack-hot-middleware/client':
+    //   path.resolve(__dirname, '../node_modules/webpack-hot-middleware/client.js'),
+    //   'react-hot-loader/patch':
+    //   path.resolve(__dirname, '../node_modules/react-hot-loader/patch.js'),
     //   'ansi-html': path.resolve(__dirname, '../node_modules/ansi-html/'),
     //   'html-entities': path.resolve(__dirname, '../node_modules/html-entities/'),
     //   'react-proxy': path.resolve(__dirname, '../node_modules/react-proxy/'),
@@ -40,6 +46,7 @@ module.exports = {
     modules: [path.resolve(__dirname, '../node_modules')],
   },
   module: {
+    // http://stackoverflow.com/questions/29883534/webpack-node-modules-css-index-js-didnt-return-a-function
     rules: [
       {
         test: /\.jsx?$/,
@@ -81,19 +88,18 @@ module.exports = {
             loader: 'style-loader',
           },
           {
-            loader: 'css?sourceMap',
+            loader: 'css-loader?sourceMap',
           },
           StringReplacePlugin.replace({
             replacements: [
               {
                 pattern: /\{\{\{(\S*?)\}\}\}/g,
-                // TODO 配置域名
-                replacement: (match, p1) => `http://piaofang.wepiao.com/static/build/${p1}`,
+                replacement: (match, p1) => `http://${domain}/${buildPath}/${p1}`,
               },
             ],
           }),
           {
-            loader: 'stylus?sourceMap',
+            loader: 'stylus-loader?sourceMap',
           },
         ],
       },
@@ -112,6 +118,5 @@ module.exports = {
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM',
-    echarts: 'echarts',
   },
 }
