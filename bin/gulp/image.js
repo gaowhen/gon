0e5af17e4906db19462b02daa4e8b43ef4337010
gaowhen <gaowhen.com@gmail.com>
2017-02-14 10:12:44 +0800

    fix: watch images in folder

--- bin/gulp/image.js
+++ bin/gulp/image.js
@@ -6,7 +6,7 @@ const config = require('../config').asset
 
 module.exports = exports = function img() {
   return gulp.src(`${config.src.img}/**/*.+(png|gif|jpg|eot|woff|ttf|svg|ico)`)
-    // 如果有同名文件，即使在文件夹内，也会因为缓存而不被上传
+    // 如果有同名文件，会因为缓存而不被上传
     // .pipe(gulpif(global.isDev, cache()))
     .pipe(gulp.dest(config.build.img))
 }
